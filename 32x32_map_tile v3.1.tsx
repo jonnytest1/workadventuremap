95d999170e703ee9f0e63229bf40f2b65173d959
jonny <ga86lad@mytum.de>
2021-04-19 19:46:39 +0200

    initialmap

---
+++ 32x32_map_tile v3.1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="32x32_map_tilev31" tilewidth="32" tileheight="32" tilecount="667" columns="23">
+ <image source="32x32_map_tile v3.1 [MARGINLESS].png" trans="ffef24" width="736" height="928"/>
+</tileset>
