fba8d5a96d4c911bf6b0cb78e7da0c09308814da
jonny <ga86lad@mytum.de>
2021-04-26 00:54:22 +0200

    added ocean map

---
+++ haus/tilesetfiles/bike1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="bike1" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="bike1.png" trans="eeeeee" width="32" height="32"/>
+</tileset>
