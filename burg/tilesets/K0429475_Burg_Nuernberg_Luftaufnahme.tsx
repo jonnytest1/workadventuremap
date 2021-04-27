7950b73ad62697f9dd70c2cb9283adb472b392aa
jonny <ga86lad@mytum.de>
2021-04-27 22:50:04 +0200

    burg

---
+++ burg/tilesets/K0429475_Burg_Nuernberg_Luftaufnahme.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="K0429475_Burg_Nuernberg_Luftaufnahme" tilewidth="32" tileheight="32" tilecount="384" columns="16">
+ <image source="K0429475_Burg_Nuernberg_Luftaufnahme.jpg" trans="eeeeee" width="512" height="512"/>
+</tileset>
--- scripts/index.d.ts
+++ scripts/index.d.ts
@@ -1,16 +1,35 @@
-/// <reference path="../../../workadventure/front/src/iframe_api.ts" />
-
 interface Popup {
     close()
 }
-
 
 interface PopupOptions {
     label: string,
     className?: "normal" | "primary" | "success" | "warning" | "error" | "disabled",
     callback?: (popup: Popup) => unknown
 }
-declare interface WorkAdventureApi {
-    getGameState
-    loadPage
+
+declare global {
+   
+    interface WorkAdventureApi {
+        sendChatMessage(message: string, author: string): void;
+        onChatMessage(callback: (message: string) => void): void;
+        onEnterZone(name: string, callback: () => void): void;
+        onLeaveZone(name: string, callback: () => void): void;
+        openPopup(targetObject: string, message: string, buttons: any[]): Popup;
+        openTab(url : string): void;
+        goToPage(url : string): void;
+        openCoWebSite(url : string): void;
+        closeCoWebSite(): void;
+        disablePlayerControl() : void;
+        restorePlayerControl() : void;
+        displayBubble() : void;
+        removeBubble() : void;
+        registerMenuCommand(commandDescriptor: string, callback: (commandDescriptor: string) => void): void
+
+        getGameState
+        loadPage
+    }
+
 }
+
+var WA:WorkAdventureApi
