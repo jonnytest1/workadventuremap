ebed085ece6b3e6f7003ff39917af266e0bea53f
jonny <ga86lad@mytum.de>
2021-05-04 22:09:08 +0200

    mapoverlay a11

--- scripts/game/overlay/mapoverlay/src/app/app.module.ts
+++ scripts/game/overlay/mapoverlay/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { FriendsComponent } from './friends/friends.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, RouterModule.forRoot(routes)
+  ],
+  declarations: [
+    AppComponent, FriendsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ scripts/game/overlay/mapoverlay/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+import { FriendsComponent } from './friends/friends.component';
+
+export const routes: Array<Route> = [{
+    path: 'friends',
+    component: FriendsComponent
+}];
