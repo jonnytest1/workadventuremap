05df9c61c851047d32925426adba67e2c674301d
jonny <ga86lad@mytum.de>
2021-05-05 12:39:07 +0200

    user update on church

--- scripts/backend-connection.js
+++ scripts/backend-connection.js
@@ -1,3 +1,4 @@
+///<reference path="./index.d.ts" />
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
         var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
