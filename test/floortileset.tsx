66dacfb460f8ca59de33b485f1577b6e0a2206d1
jonny <ga86lad@mytum.de>
2021-05-05 22:18:18 +0200

    test varioussizes

---
+++ test/floortileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="32x32_map_tile v3.1 [MARGINLESS]" tilewidth="50" tileheight="50" tilecount="252" columns="14">
+ <image source="../32x32_map_tile v3.1 [MARGINLESS].png" trans="eeeeee" width="736" height="928"/>
+</tileset>
