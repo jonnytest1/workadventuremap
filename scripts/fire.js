a2fd6a7a10374695408e1acf250d2209e7f81f0a
jonny <ga86lad@mytum.de>
2021-05-10 18:03:17 +0200

    add new item to inventory on completion

--- scripts/fire.js
+++ scripts/fire.js
@@ -1,45 +1,48 @@
 /// <reference path="index.d.ts" />
 
 const ws = require('./backend-connection');
-const zonedpp = require("./zoned-popup")
-const statePr = WA.getGameState()
+const zonedpp = require('./zoned-popup');
+const statePr = WA.getGameState();
 WA.onEnterZone('death', async () => {
     try {
         const { message } = await ws;
         const [state] = await Promise.all([statePr,
             message({
                 type: 'incrementDeath'
-            })])
+            })]);
         WA.exitSceneTo(`/${state.roomId}${state.startLayerName ? '#' + state.startLayerName : ''}`);
         WA.sendChatMessage('you died ...', '');
     } catch(e) {
-        console.error(e)
+        console.error(e);
     }
 });
 
-
-WA.onEnterZone("item-pickup", async () => {
+WA.onEnterZone('item-pickup', async () => {
     const { message } = await ws;
     const playerState = await message({
-        type: "getUserData"
-    })
+        type: 'getUserData'
+    });
     if(!playerState.gameModeEnabled) {
-        WA.openPopup("no-player", "sorry you need to complete the game-mode quest first", [{
-            label: "ok",
+        WA.openPopup('no-player', 'sorry you need to complete the game-mode quest first', [{
+            label: 'ok',
             callback: popup => {
-                popup.close()
+                popup.close();
             }
-        }])
+        }]);
     } else {
-        WA.sendChatMessage("you received some item idk no items yet give me some tips or try later", "engine")
+        WA.sendChatMessage('you received some item idk no items yet give me some tips or try later ill save it for now', 'engine');
+        message({
+            type: 'addItem',
+
+        });
     }
 });
 
 (async () => {
-    const { popupInZone } = await zonedpp
+    const { popupInZone } = await zonedpp;
     popupInZone({
-        zone: "jitsi-guide",
-        popupText: "this jitsi call is connected to openbas etage1 oben rechts in der ecke in case u need a distraction but still want to be in call :)",
+        zone: 'jitsi-guide',
+        popupText: 'this jitsi call is connected to openbas etage1 oben rechts in der ecke in case u need a distraction but still want to be in call :)',
         popupOptions: []
-    })
-})()
+    });
+})();
