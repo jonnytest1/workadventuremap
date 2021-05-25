5efb655f4c7fc9fe3721cfbc15354eb3817e29cb
jonny <ga86lad@mytum.de>
2021-05-25 14:47:57 +0200

    no resetting of livestream

--- space/display-livestream.js
+++ space/display-livestream.js
@@ -5,12 +5,12 @@ window.blockAutoOpenOverlay = true
 
 onload = () => {
     setTimeout(() => {
-        WA.openCoWebSite("../space/iss.html")
+        //WA.openCoWebSite("../space/iss.html")
         setTimeout(() => {
             WA.triggerMessage("press 'space' to jump down", () => {
                 WA.exitSceneTo("/_/global/pi4.e6azumuvyiabvs9s.myfritz.net/mapserver/rest/mapserver/site.json")
             })
-        }, 2000)
+        }, 1000)
 
 
     }, 1000);
