a13e262720699388d211cd6ffbdb4f0f55dd73b8
jonny <ga86lad@mytum.de>
2021-05-25 15:39:00 +0200

    reset livestream

--- space/display-livestream.js
+++ space/display-livestream.js
@@ -4,8 +4,9 @@ window.blockAutoOpenOverlay = true
 
 
 onload = () => {
+    WA.openCoWebSite("../space/iss.html")
     setTimeout(() => {
-        //WA.openCoWebSite("../space/iss.html")
+
         setTimeout(() => {
             WA.triggerMessage("press 'space' to jump down", () => {
                 WA.exitSceneTo("/_/global/pi4.e6azumuvyiabvs9s.myfritz.net/mapserver/rest/mapserver/site.json")
--- space/tiles/paledot.tsx
+++ space/tiles/paledot.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="paledot" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="paledot.png" trans="eeeeee" width="960" height="960"/>
+<tileset version="1.5" tiledversion="1.6.0" name="paledot" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="paledot.png" trans="eeeeee" width="480" height="480"/>
 </tileset>
