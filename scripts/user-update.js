d11d35a34f4b7f3c5c1fec6ea28a96615291de9c
jonny <ga86lad@mytum.de>
2021-06-07 11:42:16 +0200

    store zoom level

--- scripts/user-update.js
+++ scripts/user-update.js
@@ -1,17 +1,27 @@
-
+/// <reference path="./index.js" />
 
 scriptNesting(require('./backend-connection'), async imports => {
     const { message } = await imports;
     setTimeout(() => {
         WA.getGameState()
             .then(async state => {
-                await message({
-                    type: 'userUpdate',
-                    data: {
-                        pusherUuid: state.uuid,
-                        nickName: state.nickName
-                    }
-                });
+                await Promise.all([
+                    message({
+                        type: "setAttribute",
+                        data: {
+                            key: "devicePixelRatio",
+                            value: devicePixelRatio
+                        }
+                    }),
+                    message({
+                        type: 'userUpdate',
+                        data: {
+                            pusherUuid: state.uuid,
+                            nickName: state.nickName
+                        }
+                    })
+                ]);
+
 
             });
     }, 2000);
